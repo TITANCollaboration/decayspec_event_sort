9bd443b2a5cded4ecaacfa36afdcde927c3a0593
Jon Ringuette <bobskr@gmail.com>
2021-05-18 17:25:18 -0700

    Adding redis based temporal histograms in near realtime

--- hist_online.py
+++ hist_online.py
@@ -10,9 +10,13 @@ import argparse
 from lib.temporal_histogram_generator import temporal_histogram_generator
 
 
-
 def parse_and_run(args):
-    print("hi")
+    my_online_hist = temporal_histogram_generator(channel=args.channel,
+                                                  pulse_height_bin_min=args.pulse_height_bin_min,
+                                                  pulse_height_bin_max=args.pulse_height_bin_max)
+    #print(my_online_hist.get_redis_hist_entry()['unix_ts_ms'])
+    #print(my_online_hist.get_redis_queue_length())
+    print(my_online_hist.drain_redis_queue())
     return
 
 
@@ -24,6 +28,14 @@ def main():
                         help="Redis Hostname")
     parser.add_argument('--redis_port', dest='redis_port', required=False,
                         help="Redis port")
+    parser.add_argument('--redis_queue', dest='redis_queue', required=False,
+                        help="Redis queue")
+    parser.add_argument('--channel', dest='channel', type=int, required=False,
+                        help="ADC channel # to graph (0-15 for MDPP16)")
+    parser.add_argument('--ph_bin_min', dest='pulse_height_bin_min', type=int, required=False,
+                        help="Minimum bin # to count towards hit in heatmap (Range: 0-2048)")
+    parser.add_argument('--ph_bin_max', dest='pulse_height_bin_max', type=int, required=False,
+                        help="Maxmum bin # to count towards hit in heatmap (Range: 0-2048)")
 
     args, unknown = parser.parse_known_args()
     parse_and_run(args)
--- lib/temporal_histogram_generator.py
+++ lib/temporal_histogram_generator.py
@@ -8,6 +8,39 @@ import matplotlib.pyplot as plt
 import redis
 import json
 
+
 class temporal_histogram_generator:
-    def __init__(self):
+    def __init__(self, redis_hostname="localhost", redis_port="6379", redis_queue='mdpp16:queue', channel=0,
+                 pulse_height_bin_min=0, pulse_height_bin_max=2048, egun_voltage_min=2000, egun_voltage_max=2700
+                 egun_voltage_step_size=10):
+        self.redis_hostname = redis_hostname
+        self.redis_port = redis_port
+        self.redis_queue = redis_queue
+        self.channel = channel
+        self.pulse_height_bin_min = pulse_height_bin_min
+        self.pulse_height_bin_max = pulse_height_bin_max
+
+        try:
+            self.queue_conn = redis.Redis(host=self.redis_hostname, port=self.redis_port)
+        except:
+            print("Failed to connect to REDIS server")
+            exit(0)
         return
+
+    def get_queue_length(self):
+        queue_length = self.queue_conn.llen(self.redis_queue)
+        return queue_length
+
+    def get_hist_entry_from_queue(self):
+        json_from_queue = self.queue_conn.lpop(self.redis_queue)
+        myval = json.loads(json_from_queue)  # Decode the JSON contained in the queue entry into list
+        return myval
+
+    def drain_queue(self):
+        current_queue_length = 2 # self.get_queue_length(()  # Set this back after testing...
+        queue_contents = []
+        for queue_entry_index in range(0, current_queue_length):
+            queue_contents.append(self.get_hist_entry_from_queue())
+        return queue_contents
+
+#  channel, TDC, unix_TS, histogram
