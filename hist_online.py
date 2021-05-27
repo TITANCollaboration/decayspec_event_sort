816113090644ad906f47d1822a0346746d5eedf9
Jon Ringuette <bobskr@gmail.com>
2021-05-27 16:41:56 -0700

    Now processing time vs pulse height graphs

--- hist_online.py
+++ hist_online.py
@@ -12,10 +12,15 @@ from lib.temporal_histogram_generator import temporal_histogram_generator
 
 def parse_and_run(args):
     my_online_hist = temporal_histogram_generator(channel=args.channel,
-                                                  pulse_height_bin_min=args.pulse_height_bin_min,
-                                                  pulse_height_bin_max=args.pulse_height_bin_max)
+                                                  pulse_height_bin_min=int(args.pulse_height_bin_min),
+                                                  pulse_height_bin_max=int(args.pulse_height_bin_max),
+                                                  heatmap_type=args.heatmap_type)
     #my_online_hist.display_heatmap()
-    my_online_hist.online_voltage_vs_time_heatmap()
+    if args.heatmap_type == "voltage_v_time":
+        my_online_hist.online_voltage_vs_time_heatmap()
+    elif args.heatmap_type == "energy_v_time":
+        my_online_hist.online_energy_vs_time_heatmap()
+
     #print(my_online_hist.get_redis_hist_entry()['unix_ts_ms'])
     #print(my_online_hist.get_redis_queue_length())
     #print(my_online_hist.drain_redis_queue())
@@ -32,11 +37,13 @@ def main():
                         help="Redis port")
     parser.add_argument('--redis_queue', dest='redis_queue', required=False,
                         help="Redis queue")
+    parser.add_argument('--heatmap_type', dest='heatmap_type', default="voltage_v_time", required=False,
+                        help="Heatmap Types: voltage_v_time, energy_v_time")
     parser.add_argument('--channel', dest='channel', type=int, default=0, required=False,
                         help="ADC channel # to graph (0-15 for MDPP16)")
-    parser.add_argument('--ph_bin_min', dest='pulse_height_bin_min', type=int, required=False,
+    parser.add_argument('--ph_bin_min', dest='pulse_height_bin_min', type=int, default=0, required=False,
                         help="Minimum bin # to count towards hit in heatmap (Range: 0-2048)")
-    parser.add_argument('--ph_bin_max', dest='pulse_height_bin_max', type=int, required=False,
+    parser.add_argument('--ph_bin_max', dest='pulse_height_bin_max', type=int, default=2048, required=False,
                         help="Maxmum bin # to count towards hit in heatmap (Range: 0-2048)")
 
     args, unknown = parser.parse_known_args()
--- lib/temporal_histogram_generator.py
+++ lib/temporal_histogram_generator.py
@@ -13,22 +13,27 @@ import json
 class temporal_histogram_generator:
     def __init__(self, redis_hostname="localhost", redis_port="6379", redis_queue='mdpp16:queue', channel=0,
                  pulse_height_bin_min=0, pulse_height_bin_max=2048, egun_voltage_min=2000, egun_voltage_max=2700,
-                 egun_voltage_step_size=10, heatmap_type="voltage_v_time", max_time_per_cycle_ms=1000, time_per_cycle_step_size_ms=10):
-        self.time_bin_count = int(max_time_per_cycle_ms / time_per_cycle_step_size_ms)
+                 egun_voltage_step_size=10, heatmap_type="voltage_v_time", max_time_per_cycle_ms=1000, time_per_cycle_step_size_ms=10,
+                 max_time=60*1000):
         self.tdc_current = 0
         self.current_time_step = 0
-        self.current_voltage = 0 # egun_voltage_min
+        self.current_voltage = 0  # egun_voltage_min
         self.current_voltage_step = 0
         self.fig = plt.figure()
         self.first_run = True
         self.colorbar_axes = None
 
         if heatmap_type == "voltage_v_time":
+            self.time_bin_count = int(max_time_per_cycle_ms / time_per_cycle_step_size_ms)
             self.egun_voltage_bin_count = int((egun_voltage_max-egun_voltage_min) / egun_voltage_step_size)
             print("Egun Voltage bin count:", self.egun_voltage_bin_count)
             print("Time bin count:", self.time_bin_count)
             self.heatmap_2darray = np.zeros((self.time_bin_count, self.egun_voltage_bin_count), dtype=np.uint32)
+        elif heatmap_type == "energy_v_time":
+            self.time_bin_count = int(max_time / time_per_cycle_step_size_ms)
+            self.heatmap_2darray = np.zeros((self.time_bin_count, pulse_height_bin_max-pulse_height_bin_min), dtype=np.uint32)
 
+        self.max_time = max_time
         self.redis_hostname = redis_hostname
         self.redis_port = redis_port
         self.redis_queue = redis_queue
@@ -60,7 +65,7 @@ class temporal_histogram_generator:
 
     def drain_queue(self):
         current_queue_length = self.get_queue_length()
-        print("Current queue length:", current_queue_length )
+        print("Current queue length:", current_queue_length)
 #        if self.get_queue_length() != 0:
 #            current_queue_length = 1 # self.get_queue_length(()  # Set this back after testing...
 #        else:
@@ -70,10 +75,23 @@ class temporal_histogram_generator:
             queue_contents.append(self.get_hist_entry_from_queue())
         return queue_contents
 
+    def process_energy_vs_time_data(self, frame_value):  # frame value is just for the animation stuff, we don't care about it
+        queue_contents = self.drain_queue()  # Get all available entries from queue
+        for queue_entry in queue_contents:
+            #print(queue_entry['hist'][self.channel][self.pulse_height_bin_min:self.pulse_height_bin_max])
+            self.heatmap_2darray = np.delete(self.heatmap_2darray, 0, 0)  # Delete top row of 2d array, essentially pop oldest value off
+            self.heatmap_2darray = np.append(self.heatmap_2darray,
+                                         [queue_entry['hist'][self.channel][self.pulse_height_bin_min:self.pulse_height_bin_max]], axis=0)
+#    np.append(A, [[7, 8, 9]], axis=0)
+            #sum_hits_in_region = np.sum(np.array(queue_entry['hist'][self.channel])[self.pulse_height_bin_min:self.pulse_height_bin_max])
+            #print("Sum of hits", sum_hits_in_region)
+            #print("channel:", self.channel)
+        self.energy_vs_time_heatmap()
+        return
 
     def process_voltage_vs_time_raw_data(self, frame_value):
         # Get all available data from the queue (redis) then convert it to data that can be
-        # applied to the 2d array for creating the heatmap
+        # applied to the 2d array for creating the heatmap.  Frame_value does nothing right now
         queue_contents = self.drain_queue()  # Get all available entries from queue
         for queue_entry in queue_contents:
             #print("Time Diff: ", queue_entry['time_diff'])
@@ -99,17 +117,17 @@ class temporal_histogram_generator:
         self.voltage_vs_time_heatmap()
         return
 
-    def display_heatmap(self, extent):
+    def display_heatmap(self, extent, origin):
         if self.first_run is True:
             # can also put in vmin and vmax to imshow to auto scale the coloring..
-            self.im = plt.imshow(self.heatmap_2darray, cmap='viridis', extent=extent, aspect='auto', origin='lower', vmin=0, vmax=40)
+            self.im = plt.imshow(self.heatmap_2darray, cmap='viridis', extent=extent, aspect='auto', origin=origin, vmin=0, vmax=40)
             #self.colorbar = plt.colorbar(cax=self.colorbar_axes)
             self.first_run = False
 
         else:
             self.im.set_array(self.heatmap_2darray)
             # Uncomment the imshow line here to have it update the full image every time including the colors
-            #self.im = plt.imshow(self.heatmap_2darray, cmap='viridis', extent=extent, aspect='auto', origin='lower')
+            self.im = plt.imshow(self.heatmap_2darray, cmap='viridis', extent=extent, aspect='auto', origin=origin)
 
             #norm = plt.Normalize(vmin=0, vmax=500)
 
@@ -118,18 +136,30 @@ class temporal_histogram_generator:
             #plt.colorbar(self.im)
         return
 
+    def energy_vs_time_heatmap(self):
+        extent = self.pulse_height_bin_min, self.pulse_height_bin_max, 0, self.time_bin_count  # (left, right, bottom, top)
+        self.display_heatmap(extent, "upper")
+        return
+
     def voltage_vs_time_heatmap(self):
         extent = self.egun_voltage_min, self.egun_voltage_max, 0, self.max_time_per_cycle_ms  # (left, right, bottom, top)
-        self.display_heatmap(extent)
+        self.display_heatmap(extent, "lower")
         return
 
     def online_voltage_vs_time_heatmap(self):
         self.process_voltage_vs_time_raw_data(0)
         #plt.colorbar()
+        plt.xlabel("Electron Gun Energy (eV)", fontsize=20)
+        plt.ylabel("Time", fontsize=20)
         ani = animation.FuncAnimation(self.fig, self.process_voltage_vs_time_raw_data, interval=1000)
         plt.show()
 
-    def pulse_height_vs_time_heatmap(self):
+    def online_energy_vs_time_heatmap(self):
+        self.process_energy_vs_time_data(0)
+        plt.xlabel("Pulse Height", fontsize=20)
+        plt.ylabel("Time", fontsize=20)
+        ani = animation.FuncAnimation(self.fig, self.process_energy_vs_time_data, interval=1000)
+        plt.show()
         return
 
 
