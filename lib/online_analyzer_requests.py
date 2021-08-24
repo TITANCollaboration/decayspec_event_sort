cd7e61497d9663ab928b0e6b8c3bb87662c7b5ee
Jon Ringuette <bobskr@gmail.com>
2021-08-24 09:40:51 -0600

    bug fixes

--- lib/online_analyzer_requests.py
+++ lib/online_analyzer_requests.py
@@ -1,6 +1,6 @@
 import requests
 import pandas as pd
-
+import json
 
 class online_analyzer_requests:
     def __init__(self):
@@ -9,14 +9,18 @@ class online_analyzer_requests:
         return
 
     def fetch_remote_hist(self, channels):
-        payload = {'cmd': 'callspechandler', 'spectrum1': 'mdpp16_1_Pulse_Height'}
+        payload = {'cmd': 'callspechandler', 'spectrum0': 'mdpp16_0_Pulse_Height'}
+        for index, my_channel in enumerate(channels):
+            spectrum = 'spectrum' + str(index)
+            payload[spectrum] = my_channel
+        print("Payload:", payload)
         try:
-            r = requests.get(self.remote_analyzer_url, params=payload, timeout=1)
-            print(r)
-            mydata_df = pd.DataFrame(data={'0': []})
+            r = requests.get(self.remote_analyzer_url, params=payload, timeout=2)
             status = 0
         except:
             print("Timeout or other connection issue.")
             mydata_df = pd.DataFrame(data={'0': []})
             status = 1  # Timeout or connection issue
+        mydata = r.text.replace("'", '"')  # For whatever reason the data coming out of the online analyzer only has single quotes where as it should have double.  I'm doing this instead of fixing the analyzer to make sure the older spectrum viewer will still work.
+        mydata_df = pd.DataFrame(data=json.loads(mydata))
         return mydata_df, status
