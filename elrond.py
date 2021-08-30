a2e327468f070c137e0456761bd2cbfb354050c4
Jon Ringuette <bobskr@gmail.com>
2021-08-30 16:06:47 -0700

    Can now select graph to fit, PH vs time histos getting closer

--- elrond.py
+++ elrond.py
@@ -108,11 +108,12 @@ offline_tab_content = html.Div([html.Div(className='clear'),
                                 html.Button('Plus', id='add-file-hist', n_clicks=0),
                                 html.Button('Minus', id='remove-file-hist', n_clicks=0),
                                 html.Div(id='hist-dropdown-list', children=[]),
-                                html.Div(className='clear')
+                                html.Div(className='clear'),
                                 ])
 
 online_tab_content = html.Div([
-                        channel_selections_from_daqs])
+                        channel_selections_from_daqs,
+                        html.Div(id={'type': 'temporal_graph', 'index': 'my_temporal_graph_index'})])
 
 header = html.Header([html.Span('GammaGraph', style={'float': 'left'}),
                       html.Span(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), style={'float': 'right'}),
@@ -148,14 +149,13 @@ app.layout = html.Div([
     tabbed_modes,
     dcc.Store(id='tab-mode-selection'),
     dcc.Store(id='hist_filename'),
+    dcc.Store(id='graphing_online_ph_vs_time'),
     html.Div(id='tabs-content-inline'),
     html.Hr(),
     html.Div([dcc.Graph(id='hist_graph_display')]),
+    html.Div(dcc.Interval(id='temporal-hist-interval-component', interval=999999999)),
     html.Div(dcc.Interval(id='interval-component', interval=999999999)),
-    #html.Div([html.Button('Fit Peak', id='fit-peak-button', n_clicks=0),
-    #          dcc.Store(id='peak_fit_first_point')]),
-
-    html.Div([html.Button('Fit Peak', id='fit-peak-button', n_clicks=0, value='testme_chan_100'),
+    html.Div([html.Button('Fit Peak', id='fit-peak-button', value=None, n_clicks=0),
               dcc.Store(id='peak_fit_first_point')]),
     html.Div(id='channel_list_with_info', children=[]),
 ])
@@ -174,12 +174,50 @@ def render_tab_content(tab_selection, tab_mode):
         return online_tab_content, tab_mode
 
 
+@app.callback(Output({'type': 'temporal_graph', 'index': ALL}, 'children'),
+              Output('temporal-hist-interval-component', 'interval'),
+              Output('graphing_online_ph_vs_time', 'data'),
+              Output({'type': 'pulse_height_vs_time', 'index': ALL}, 'n_clicks'),
+              Input('temporal-hist-interval-component', 'n_intervals'),
+              Input('temporal-hist-interval-component', 'interval'),
+              Input({'type': 'pulse_height_vs_time', 'index': ALL}, 'n_clicks'),
+              Input({'type': 'selected_chan_dropdown', 'index': ALL}, 'value'),
+              Input({'type': 'temporal_graph', 'index': ALL}, 'children'),
+              State('graphing_online_ph_vs_time', 'data'),
+              )
+def online_temporal_histogram(temporal_hist_interval_component, temporal_hist_interval_time, pulse_height_vs_time_nclicks, selected_chan_dropdown_value, temporal_graph_children, graphing_online_ph_vs_time_data):
+    temporal_hist_interval_time = temporal_hist_interval_time
+    #  Check if mdpp16_0 is in the list, there are some problems here but oh well, also make sure we aren't
+    #  already performing a temporal histogram plot
+    pulse_height_vs_time_n_clicks_output = []
+
+    if ('mdpp16_0' in str(selected_chan_dropdown_value)) and (graphing_online_ph_vs_time_data <= 0):
+        temporal_graph_children[0] = [html.Button('PH v. Time', id={'type': 'pulse_height_vs_time', 'index': 'pulse_height_vs_time'}, n_clicks=0)]
+    if pulse_height_vs_time_nclicks:  # Check if it exists or if this is just the init run
+        if (pulse_height_vs_time_nclicks[0] == 1):  # Check if the button was pushed
+            pulse_height_vs_time_n_clicks_output =  pulse_height_vs_time_nclicks
+            pulse_height_vs_time_n_clicks_output[0] = pulse_height_vs_time_n_clicks_output[0] - 1
+
+            graphing_online_ph_vs_time_data = graphing_online_ph_vs_time_data + 1  # if it was incriment the stored variable
+            if graphing_online_ph_vs_time_data % 2 == 0:  # if it's even shut off the graph and interval timer
+                temporal_hist_interval_time = 999999999
+                temporal_graph_children[0] = [html.Button('PH v. Time', id={'type': 'pulse_height_vs_time', 'index': 'pulse_height_vs_time'}, n_clicks=0)]
+            else:
+                temporal_graph_children[0].append(dcc.Graph(id='online_ph_v_time_hist_graph'))
+                temporal_hist_interval_time = 5*1000
+    else:
+        graphing_online_ph_vs_time_data = 0
+    if graphing_online_ph_vs_time_data > 0:  # Check if we've set stuff one and need to handle this differently
+        pulse_height_vs_time_n_clicks_output = pulse_height_vs_time_nclicks
+
+    return temporal_graph_children, temporal_hist_interval_time, graphing_online_ph_vs_time_data, pulse_height_vs_time_n_clicks_output
+
 @app.callback(
     Output('xmin', 'value'),
     Output('xmax', 'value'),
     Input('hist_graph_display', 'relayoutData'))
 def zoom_event(relayoutData):
-    print(relayoutData)
+    #print(relayoutData)
     if relayoutData is None:
         raise dash.exceptions.PreventUpdate
 
@@ -231,9 +269,9 @@ def create_hist_file_dropdown(add_nclicks, remove_nclicks, hist_dropdown):
     State('hist_filename', 'data'),
 )
 def set_static_hist_filename_w_chan_selection(hist_file_selection, hist_available_channel_dropdown, chan_index, stored_hist_filename):
-    print("Chan index! :", chan_index)
-    print("Hist selection", hist_file_selection)
-    print("hist_available_channel_dropdown", hist_available_channel_dropdown)
+    #print("Chan index! :", chan_index)
+    #print("Hist selection", hist_file_selection)
+    #print("hist_available_channel_dropdown", hist_available_channel_dropdown)
     stored_hist_filename = hist_file_selection
     #hist_file_selection = hist_file_selection[0]
     print("Stored value", stored_hist_filename)
@@ -256,7 +294,6 @@ def set_static_hist_filename_w_chan_selection(hist_file_selection, hist_availabl
                                                             multi=True,
                                                             placeholder="Select Channel")
                                                             ])
-    print("hist_available_channel_dropdown AFTER", hist_available_channel_dropdown)
     return stored_hist_filename, hist_available_channel_dropdown
 
 @app.callback(
@@ -266,6 +303,7 @@ def set_static_hist_filename_w_chan_selection(hist_file_selection, hist_availabl
     Output('fit-peak-button', 'n_clicks'),
     Output('hist_graph_display', 'clickData'),
     Output('channel_list_with_info', 'children'),
+    Output('fit-peak-button', 'value'),
     Input('interval-component', 'n_intervals'),
     Input('yaxis-type', 'value'),
     Input("xmin", "value"),
@@ -274,19 +312,22 @@ def set_static_hist_filename_w_chan_selection(hist_file_selection, hist_availabl
     Input({'type': 'selected_chan_dropdown', 'index': ALL}, 'value'),
     Input('fit-peak-button', 'n_clicks'),
     Input('fit-peak-button', 'value'),
+    Input({'type': 'fit_chan_radio', 'index': ALL}, 'value'),
     Input('hist_graph_display', 'clickData'),
     Input('channel_list_with_info', 'children'),
     State('peak_fit_first_point', 'data'),
     State('hist_filename', 'data'),
     State('tab-mode-selection', 'data'),
+
     )
-def process_static_hist(n_intervals, yaxis_type, xmin, xmax, unzoom, hist_available_channel_list, fit_peak_button, fit_peak_button_value, click_data, channel_list_with_info, stored_data, stored_hist_filename, tab_mode_selection):
+def process_static_hist(n_intervals, yaxis_type, xmin, xmax, unzoom, hist_available_channel_list, fit_peak_button, fit_peak_button_value, fit_chan_radio_value, click_data, channel_list_with_info, stored_data, stored_hist_filename, tab_mode_selection, ):
+    if fit_chan_radio_value:
+        fit_peak_button_value = fit_chan_radio_value[0]
+    else:
+        fit_peak_button_value = None
     triggered = [t["prop_id"] for t in dash.callback_context.triggered]
-    print("Fit peak button:", fit_peak_button)
-    print("Fit peak button value", fit_peak_button_value)
+
     print("Triggerd:", triggered)
-    print("Tab mode:", tab_mode_selection)
-    print("Channels we should be looking at:", hist_available_channel_list, "Filename", stored_hist_filename)
     update_interval = 9999999999  # Set this to a long period of time assuming we're in offline mode, change it only when we are definitly in online mode with channels selected.
 
     channels_to_display = []
@@ -295,14 +336,13 @@ def process_static_hist(n_intervals, yaxis_type, xmin, xmax, unzoom, hist_availa
             channels_to_display.extend(channels)
 
     changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]  # determines what property changed, in this case which button was pushed
-    print("Change id", changed_id)
+    #print("Change id", changed_id)
     hist_file_selection = stored_hist_filename
 
     if len(channels_to_display) > 0:
         if tab_mode_selection == "offline-analysis":
             if hist_file_selection is None:
                 raise dash.exceptions.PreventUpdate
-            print("Offline Graphing mode...")
             mydata_df = pd.DataFrame()
             for hist_file_enum in enumerate(stored_hist_filename):
                 if hist_file_enum[1] is not None:
@@ -311,7 +351,6 @@ def process_static_hist(n_intervals, yaxis_type, xmin, xmax, unzoom, hist_availa
                         new_column_name = new_chanel_name(hist_file_enum[1], my_column_name)
                         tmp_df.rename(columns={my_column_name: new_column_name}, inplace=True)
                     mydata_df = pd.concat([mydata_df, tmp_df], axis=1).fillna(0)
-            print("hist available list:", hist_available_channel_list)
         else:
             mydata_df, channels_to_display, status = remote_online_df(channels_to_display, "Pulse_Height")
             update_interval = 5*1000
@@ -319,7 +358,7 @@ def process_static_hist(n_intervals, yaxis_type, xmin, xmax, unzoom, hist_availa
             xmin = 0
             xmax = len(mydata_df.index)
         fig_hist = create_histogram(mydata_df, channels_to_display, yaxis_type, xmin, xmax)
-        channel_list_with_info = generate_channel_list_with_info(mydata_df, tab_mode_selection, channels_to_display)
+        channel_list_with_info = generate_channel_list_with_info(mydata_df, tab_mode_selection, channels_to_display, fit_peak_button_value)
         fig_hist.update_layout(clickmode="none")  #  Disable line based selection on graph unless driven by event
         fig_hist.update_layout(hovermode='x')  # This gives a little box with info when hovering over data
     else:
@@ -327,9 +366,9 @@ def process_static_hist(n_intervals, yaxis_type, xmin, xmax, unzoom, hist_availa
 
     if fit_peak_button == 1:
         #  !!Currently I'm only caring about one channel, need to have that be selectable!
-        stored_data, fig_hist, update_interval, fit_peak_button, click_data = fit_peak_button_mode(click_data, stored_data, fig_hist, mydata_df, channels_to_display[0])
+        stored_data, fig_hist, update_interval, fit_peak_button, click_data = fit_peak_button_mode(click_data, stored_data, fig_hist, mydata_df, fit_peak_button_value)
 
-    return fig_hist, stored_data, update_interval, fit_peak_button, click_data, channel_list_with_info
+    return fig_hist, stored_data, update_interval, fit_peak_button, click_data, channel_list_with_info, fit_peak_button_value
 
 
 @app.callback(Output('output-data-upload', 'children'),
@@ -339,7 +378,6 @@ def process_static_hist(n_intervals, yaxis_type, xmin, xmax, unzoom, hist_availa
 def save_uploaded_hist(list_of_contents, list_of_names, list_of_dates):
     # Write contents of uploaded histogram to disk after properly decoding from base64
     if list_of_contents is not None:
-        print(list_of_names)
         for hist_filename, contents in zip(list_of_names, list_of_contents):
             content_type, content_string = contents.split(',')
 
@@ -350,19 +388,19 @@ def save_uploaded_hist(list_of_contents, list_of_names, list_of_dates):
             output_file.close()
     return
 
-def generate_channel_list_with_info(mydata_df, tab_mode_selection, channels_to_display):
+def generate_channel_list_with_info(mydata_df, tab_mode_selection, channels_to_display, fit_peak_button_value):
     channel_info_list_children = []
     chan_radio_options = []
     hit_count = []
     for channel in channels_to_display:
-        chan_radio_options.append({"label": channel, "value": (channel + "_fit")})
+        chan_radio_options.append({"label": channel, "value": (channel)})
         hit_count.append(mydata_df[channel].sum())
-
     channel_info_list_children.append(
                                 html.Div([html.H4("Fit", className='row'),
-                                    dcc.RadioItems(
+                                    dcc.RadioItems({'type': 'fit_chan_radio', 'index': 'value'},
                                             options=chan_radio_options,
                                             labelClassName='row',
+                                            value=fit_peak_button_value,
                                             labelStyle={'display': 'block'}),
                                           ], className='column'))
     hit_count_children = []
@@ -373,33 +411,7 @@ def generate_channel_list_with_info(mydata_df, tab_mode_selection, channels_to_d
     channel_info_list_children.append(html.Div(className='column', children=hit_count_children))
 
     return channel_info_list_children
-"""
-def generate_channel_list_with_info(mydata_df, tab_mode_selection, channels_to_display):
-    channel_info_list_children = []
-    chan_radio_options = []
-    hit_count = []
-    for channel in channels_to_display:
-        chan_radio_options.append({"label": channel, "value": (channel + "_fit")})
-        hit_count.append(mydata_df[channel].sum())
 
-    channel_info_list_children.append(
-                                html.Div([html.H2("Fit", className='grid-item1'),
-                                    dcc.RadioItems(
-                                            options=chan_radio_options,
-                                            className='grid-item1',
-                                            labelClassName='grid-item1',
-                                            labelStyle={'display': 'block'})
-                                          ], className='grid-container'))
-    hit_count_children = []
-    hit_count_children.append(html.H2("Hit Count", className='grid-item2'))
-    for channel_sum in hit_count:
-        hit_count_children.append(html.Div(html.Div(channel_sum)))
-
-    channel_info_list_children.append(html.Div(className='grid-item2', children=hit_count_children))
-
-    print(channel_info_list_children)
-    return channel_info_list_children
-"""
 
 def new_chanel_name(filename, channel):
     file_path = Path(filename)  # Setup to extract filename prefix (stem)
@@ -419,9 +431,7 @@ def create_histogram(mydata_df, channels_to_display, yaxis_type, xmin, xmax):
     print("Do we get to static hist creation?")
     for channel in channels_to_display:
         if channel not in mydata_df.columns:
-            print("We're removing it!!")
             channels_to_display.remove(channel)
-    print("in hist creation:", channels_to_display)
 
     fig_hist = px.line(mydata_df[channels_to_display][xmin:xmax],
                               line_shape='hv',
@@ -447,7 +457,6 @@ def fit_peak_button_mode(click_data, stored_data, fig_hist, mydata_df, channel):
                            legend=dict(title=None),
                            hoverlabel=dict(bgcolor='rgba(255,255,255,0.75)',
                            font=dict(color='black')))
-    print("!!!!Fit function called!!!!")
     fig_hist.update_layout(clickmode='event+select')
     # fig_hist.update_layout(hoverlabel=dict(bgcolor="black", font_size=12,))
 
