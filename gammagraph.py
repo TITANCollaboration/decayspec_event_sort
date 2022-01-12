8e7b4582bd5638f38356b6672295dec64cb4ed31
Jon Ringuette <bobskr@gmail.com>
2022-01-12 14:09:52 -0700

    Change to allow for outputting a hist file even when 0 hits detected.  Fixes bug with multiple files and last having no hits.

--- gammagraph.py
+++ gammagraph.py
@@ -721,4 +721,4 @@ def remote_online_df(channels_to_display, type):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=8051)
+    app.run_server(debug=True, host='0.0.0.0', port=8052)
